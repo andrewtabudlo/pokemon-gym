7757b129177174a4d2e31f58b37385a4cba6d499
ronen.goren <ronen.goren@gmail.com>
2018-04-02 15:57:22 -0400

    added ronen trainer and pokemons id and fixed syntax

--- js/main.js
+++ js/main.js
@@ -32,4 +32,4 @@ function getPokemon(id, trainer){
 
 // get pokemon data
 // add your pokemon and trainer here to call your pokemon
-$.when(getPokemon(384, daenerys),getPokemon(149, daenerys),getPokemon(373, daenerys)getPokemon(18, jeddex),getPokemon(31, jeddex), getPokemon(24, jeddex)).done(render);
+$.when(getPokemon(384, daenerys),getPokemon(149, daenerys),getPokemon(373, daenerys),getPokemon(18, jeddex),getPokemon(31, jeddex), getPokemon(24, jeddex)).done(render);
