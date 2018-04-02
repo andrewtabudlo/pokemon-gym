2fd2edfdd65edc5d5a4ec039fca7689e153a72e4
Andrew Tabudlo <andrew.tabudlo@gmail.com>
2018-04-02 15:36:43 -0400

    add ajax call code to main.js

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// create new trainer
+let daenerys = new Trainer();
+// TODO do this for your trainer the same way
+
+function getPokemon(id, trainer){
+    return $.ajax({
+    url: `https://pokeapi.co/api/v2/pokemon/${id}/`,
+    dataType: 'json',
+    method: 'GET',
+    })
+    .done(function(data) {
+        console.log(data);
+        let types = [];
+        let getTypes = function(){
+            for (let i = 0; i < data.types.length; i++){
+                types.push(data.types[i].type.name);
+            }
+        }
+        getTypes();
+        let abilities = [];
+        let getAbilities = function(){
+            for (let i = 0; i < data.abilities.length; i++){
+                abilities.push(data.abilities[i].ability.name);
+            }
+        }
+        getAbilities();
+        let pokemon = new Pokemon(data.id,data.name,data.sprites.front_default,types,data.weight,data.height,data.stats[3].base_stat,data.stats[4].base_stat,data.stats[5].base_stat,abilities);
+        trainer.addPokemon(pokemon);
+    });
+}
+
+// get pokemon data
+// add your pokemon and trainer here to call your pokemon
+$.when(getPokemon(384, daenerys),getPokemon(149, daenerys),getPokemon(373, daenerys)).done(render);
