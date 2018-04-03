d851640235d64b51971dac4b40f0a69ba339c499
andrewtabudlo <andrew.tabudlo@gmail.com>
2018-04-02 21:58:34 -0400

    ronen pokemon showing checkpoint

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // create new trainer
+// TODO do this for your trainer the same way
 let urien = new Trainer();
 let mbison = new Trainer();
-// TODO do this for your trainer the same way
 
+// get json and extract data to objects, then place in respective trainers
 function getPokemon(id, trainer){
     return $.ajax({
     url: `https://pokeapi.co/api/v2/pokemon/${id}/`,
@@ -30,10 +31,33 @@ function getPokemon(id, trainer){
     });
 }
 
-function render() {
-	
+function render(trainer) {
+    if (trainer == mbison) { 
+      for (i = 0; i < trainer.pokemon.length; i++) {
+        $('#pokemonPic' + (i+1)).attr('src',trainer.pokemon[i].image);
+        $('#cardTitle' + (i+1)).html('#' + trainer.pokemon[i].id + ' ' + trainer.pokemon[i].name);
+        $('#statsList' + (i+1)).html(`<li class='collection-item'>HP: ${trainer.pokemon[i].hp}</li><li class='collection-item'>Attack: ${trainer.pokemon[i].attack}</li><li class='collection-item'>Defense: ${trainer.pokemon[i].defense}</li>`);
+        trainer.pokemon[i].abilities.forEach(function(ability) {
+          $('#abilitiesList' + (i+1)).append(`<li class='collection-item'>${ability}</li>`);
+        });
+        $('#miscList' + (i+1)).html(`<li class='collection-item'>Height: ${trainer.pokemon[i].height}</li><li class='collection-item'>Weight: ${trainer.pokemon[i].weight}</li>`);
+    }
+      } else if (trainer == urien) {
+      for (i = 3; i < (trainer.pokemon.length - 3); i++) {
+        $('#pokemonPic' + i).attr('src',trainer.pokemon[(i-3)].image);
+        $('#cardTitle' + i).html('#' + trainer.pokemon[(i-3)].id + ' ' + trainer.pokemon[(i-3)].name);
+        $('#statsList' + i).html(`<li class='collection-item'>HP: ${trainer.pokemon[(i-3)].hp}</li>`);
+        trainer.pokemon[(i-3)].abilities.forEach(function(ability) {
+          $('#abilitiesList' + i).append(`<li class='collection-item'>${ability}</li>`);
+        });
+        $('#miscList' + i).html(`<li class='collection-item'>Height: ${trainer.pokemon[(i-3)].height}</li><li class='collection-item'>Weight: ${trainer.pokemon[(i-3)].weight}</li>`);
+    }
+      } else {
+      console.log('something went wrong');
+  } 
 }
+
 
 // get pokemon data
 // add your pokemon and trainer here to call your pokemon
-$.when(getPokemon(384, urien),getPokemon(149, urien),getPokemon(373, urien),getPokemon(18, mbison),getPokemon(31, mbison), getPokemon(24, mbison)).done(render);
+$.when(getPokemon(384, urien),getPokemon(149, urien),getPokemon(373, urien),getPokemon(18, mbison),getPokemon(31, mbison), getPokemon(24, mbison)).done(render(mbison),render(urien));
