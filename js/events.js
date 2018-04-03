82d9f4540ac15c74e5d8ef060171672c09dbcc4c
Andrew Tabudlo <andrew.tabudlo@gmail.com>
2018-04-03 15:23:03 -0400

    style loading bar and text

--- js/events.js
+++ js/events.js
@@ -1,5 +1,5 @@
 // lock screen before json data is loaded
-// $('body').css({'overflow':'hidden'});
-// $(document).bind('scroll',function() {
-//	window.scrollTo(0,0);
-// });
+$('body').css({'overflow':'hidden'});
+$(document).bind('scroll',function() {
+	window.scrollTo(0,0);
+});
